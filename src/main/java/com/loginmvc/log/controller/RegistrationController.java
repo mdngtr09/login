94c106b9ec85661f858802fbf4f89cfc34850aaa
murillo dias nunes <mdngtr09@gmail.com>
2025-01-13 01:20:10 -0300

    bash

---
+++ src/main/java/com/loginmvc/log/controller/RegistrationController.java
@@ -0,0 +1,25 @@
+package com.loginmvc.log.controller;
+
+import com.loginmvc.log.model.AppUser;
+import com.loginmvc.log.model.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping(value = "/req/signup", consumes = "application/json")
+    public AppUser createUser(@RequestBody AppUser user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return appUserRepository.save(user);
+    }
+
+}
---
+++ src/main/java/com/loginmvc/log/model/AppUserRepository.java
@@ -0,0 +1,8 @@
+package com.loginmvc.log.model;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long>{
+    Optional<AppUser> findByUsername(String username);
+}
